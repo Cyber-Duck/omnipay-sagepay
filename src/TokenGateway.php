a95a65383a4a80d1ab47a70ed1e0a2e33b1a780a
luke <luke@localhost.localdomain>
2016-06-14 08:47:11 +0100

    Changes for PSR2 standard

--- src/TokenGateway.php
+++ src/TokenGateway.php
@@ -1,34 +1,48 @@
 <?php
-
-/*
- * Variation of the Server Gateway
- * TODO: Variation of the Direct Gateway
+/**
+  * TODO: Variation of the Direct Gateway, possibly factory
+  * 
+  * @category Token_Integration_Method
+  * @package Omnipaysagepay
+  * @author llincoln <buddhalincoln@gmail.com>
+  * @license https://raw.githubusercontent.com/llincoln/omnipay-sagepay/master/LICENSE dwtful
+  * @link https://github.com/llincoln/omnipay-sagepay
  */
 namespace Omnipay\SagePay;
+/**
+ * Variation of the Server Gateway
+ * 
+ */
+class TokenGateway extends \Omnipay\SagePay\ServerGateway
+{
 
-class TokenGateway extends \Omnipay\SagePay\ServerGateway {
-
-    public function getName() {
+    public function getName()
+    {
         return 'Sage Pay Server/Token';
     }
 
-    public function authorize(array $parameters = array()) {
+    public function authorize(array $parameters = array())
+    {
         return $this->createRequest('\Omnipay\SagePay\Message\TokenAuthorizeRequest', $parameters);
     }
 
-    public function completeAuthorize(array $parameters = array()) {
+    public function completeAuthorize(array $parameters = array())
+    {
         return $this->createRequest('\Omnipay\SagePay\Message\TokenCompleteAuthorizeRequest', $parameters);
     }
 
-    public function purchase(array $parameters = array()) {
+    public function purchase(array $parameters = array())
+    {
         return $this->createRequest('\Omnipay\SagePay\Message\TokenPurchaseRequest', $parameters);
     }
 
-    public function capture(array $parameters = array()) {
+    public function capture(array $parameters = array())
+    {
         return $this->createRequest('\Omnipay\SagePay\Message\TokenRequest', $parameters);
     }
 
-    public function refund(array $parameters = array()) {
+    public function refund(array $parameters = array())
+    {
         return $this->createRequest('\Omnipay\SagePay\Message\TokenRefundRequest', $parameters);
     }
 
