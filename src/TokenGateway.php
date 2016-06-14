e9752ecf0af3d49df17ac163b1aeb883e3c8da1d
luke <luke@localhost.localdomain>
2016-06-14 09:26:56 +0100

    Changes for PSR2 standard, altered netbeans formatter settings hopefully this will solve it

--- src/TokenGateway.php
+++ src/TokenGateway.php
@@ -21,32 +21,32 @@ class TokenGateway extends \Omnipay\SagePay\ServerGateway
 
     public function getName()
     {
-	return 'Sage Pay Server/Token';
+        return 'Sage Pay Server/Token';
     }
 
     public function authorize(array $parameters = array())
     {
-	return $this->createRequest('\Omnipay\SagePay\Message\TokenAuthorizeRequest', $parameters);
+        return $this->createRequest('\Omnipay\SagePay\Message\TokenAuthorizeRequest', $parameters);
     }
 
     public function completeAuthorize(array $parameters = array())
     {
-	return $this->createRequest('\Omnipay\SagePay\Message\TokenCompleteAuthorizeRequest', $parameters);
+        return $this->createRequest('\Omnipay\SagePay\Message\TokenCompleteAuthorizeRequest', $parameters);
     }
 
     public function purchase(array $parameters = array())
     {
-	return $this->createRequest('\Omnipay\SagePay\Message\TokenPurchaseRequest', $parameters);
+        return $this->createRequest('\Omnipay\SagePay\Message\TokenPurchaseRequest', $parameters);
     }
 
     public function capture(array $parameters = array())
     {
-	return $this->createRequest('\Omnipay\SagePay\Message\TokenRequest', $parameters);
+        return $this->createRequest('\Omnipay\SagePay\Message\TokenRequest', $parameters);
     }
 
     public function refund(array $parameters = array())
     {
-	return $this->createRequest('\Omnipay\SagePay\Message\TokenRefundRequest', $parameters);
+        return $this->createRequest('\Omnipay\SagePay\Message\TokenRefundRequest', $parameters);
     }
 
 }
