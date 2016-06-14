7fd76bcba5b9cf4fef0ba425a1e5516f58869383
luke <luke@localhost.localdomain>
2016-06-14 09:21:55 +0100

    Changes for PSR2 standard, altered netbeans formatter settings hopefully this will solve it

--- src/TokenGateway.php
+++ src/TokenGateway.php
@@ -1,13 +1,15 @@
 <?php
+
 /**
-  * TODO: Variation of the Direct Gateway, possibly factory
-  * 
-  * @category Token_Integration_Method
-  * @package Omnipaysagepay
-  * @author llincoln <buddhalincoln@gmail.com>
-  * @license https://raw.githubusercontent.com/llincoln/omnipay-sagepay/master/LICENSE dwtful
-  * @link https://github.com/llincoln/omnipay-sagepay
+ * TODO: Variation of the Direct Gateway, possibly factory
+ * 
+ * @category Token_Integration_Method
+ * @package Omnipaysagepay
+ * @author llincoln <buddhalincoln@gmail.com>
+ * @license https://raw.githubusercontent.com/llincoln/omnipay-sagepay/master/LICENSE dwtful
+ * @link https://github.com/llincoln/omnipay-sagepay
  */
+
 namespace Omnipay\SagePay;
 
 /**
@@ -19,33 +21,32 @@ class TokenGateway extends \Omnipay\SagePay\ServerGateway
 
     public function getName()
     {
-        return 'Sage Pay Server/Token';
+	return 'Sage Pay Server/Token';
     }
 
     public function authorize(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\SagePay\Message\TokenAuthorizeRequest', $parameters);
+	return $this->createRequest('\Omnipay\SagePay\Message\TokenAuthorizeRequest', $parameters);
     }
 
     public function completeAuthorize(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\SagePay\Message\TokenCompleteAuthorizeRequest', $parameters);
+	return $this->createRequest('\Omnipay\SagePay\Message\TokenCompleteAuthorizeRequest', $parameters);
     }
 
     public function purchase(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\SagePay\Message\TokenPurchaseRequest', $parameters);
+	return $this->createRequest('\Omnipay\SagePay\Message\TokenPurchaseRequest', $parameters);
     }
 
     public function capture(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\SagePay\Message\TokenRequest', $parameters);
+	return $this->createRequest('\Omnipay\SagePay\Message\TokenRequest', $parameters);
     }
 
     public function refund(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\SagePay\Message\TokenRefundRequest', $parameters);
+	return $this->createRequest('\Omnipay\SagePay\Message\TokenRefundRequest', $parameters);
     }
 
-    
 }
