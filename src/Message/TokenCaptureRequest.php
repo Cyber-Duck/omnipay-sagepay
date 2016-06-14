cec69a696c17433223701a65c88bb4b016e30be9
luke <luke@localhost.localdomain>
2016-06-14 10:58:16 +0100

    Corrected incorrect class name

--- src/Message/TokenCaptureRequest.php
+++ src/Message/TokenCaptureRequest.php
@@ -10,6 +10,9 @@ class TokenCaptureRequest extends \Omnipay\SagePay\Message\CaptureRequest
     {
         $data = parent::getBaseData();
         $data['VPSProtocol'] = '3.00';
+        /**
+         * Modified Here to create token and store token
+         */
         $data['CreateToken'] = 1;
         $data['StoreToken'] = 1;
         return $data;
